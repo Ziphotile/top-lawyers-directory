d2fca185dae3f7cf797d1f3644c982cd9bda39d9
BossTata <z.tile91@gmail.com>
2025-07-21 18:06:55 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LawyerDirectory from "./LawyerDirectory";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LawyerDirectory />
+  </React.StrictMode>
+);
