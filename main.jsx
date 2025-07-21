576992101c941f3a5a371c9d08dda8ba4aa24cb8
BossTata <z.tile91@gmail.com>
2025-07-21 17:13:35 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LawyerDirectory from "./LawyerDirectory";
+import "./index.css"; // Optional: for Tailwind or global styles
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LawyerDirectory />
+  </React.StrictMode>
+);
