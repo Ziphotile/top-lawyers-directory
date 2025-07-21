9e857b08dce92bbf1d9b95807ef05ab311926950
BossTata <z.tile91@gmail.com>
2025-07-21 18:06:24 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LawyerDirectory from "./LawyerDirectory";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LawyerDirectory />
-  </React.StrictMode>
-);
